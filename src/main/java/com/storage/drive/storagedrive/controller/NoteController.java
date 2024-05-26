fb5eaf0c4a8352a93a08398e694d1ccefff43461
mustafakepenek <106611584+mustafakepenek@users.noreply.github.com>
2024-05-26 18:02:18 +0300

    Update NoteController.java imports
    Some imports added to work

--- src/main/java/com/storage/drive/storagedrive/controller/NoteController.java
+++ src/main/java/com/storage/drive/storagedrive/controller/NoteController.java
@@ -1,5 +1,13 @@
 package com.storage.drive.storagedrive.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
+
 import com.storage.drive.storagedrive.model.Note;
 import com.storage.drive.storagedrive.services.NoteService;
 import com.storage.drive.storagedrive.services.UserService;
@@ -9,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
 @Controller
 public class NoteController {
